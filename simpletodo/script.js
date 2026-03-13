a5b92b38e8476d38b81a9cda046f082aeb510421
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-03-13 22:06:10 +0545

    add styling to todo list

--- simpletodo/script.js
+++ simpletodo/script.js
@@ -4,8 +4,17 @@ const btn =document.getElementById('btn');
 
 btn.addEventListener('click',function(){
 	const li = document.createElement('li');
+	const del = document.createElement('button');
 	li.textContent = userInput.value;
+	del.textContent = 'X';
+	li.classList.add('list-items');
+	del.classList.add('del-button');
+	li.appendChild(del);
 	list.appendChild(li);
+	del.addEventListener('click',function(e){
+		e.stopPropagation();
+		li.remove();
+	})
 	userInput.value = "";
 	li.addEventListener('click',function(){
 		li.classList.toggle('done');
